8ef9ca9423d238da6fab10aaf4f7556f33257cc2
KandimallaSandeep13 <142970423+KandimallaSandeep13@users.noreply.github.com>
2023-09-20 12:51:56 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -3,7 +3,7 @@
 new changes are commited
 
 echo "This is an Samples Script from  GitHub"
-echo "This is test from Linux new latest Server"
+echo "This is test from Linux new latest  new Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
